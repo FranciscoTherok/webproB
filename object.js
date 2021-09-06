b5caf719750be35b16744aff275d4b2ca269055a
reymonr <reymonr@unklab.ac.id>
2021-09-06 15:04:19 +0800

    initial commit

---
+++ object.js
@@ -0,0 +1,21 @@
+// object - literal
+const student = {
+  nim: 105011810001,
+  regNo: 's180001',
+  prodi: 'SI',
+  faculty: 'FIK',
+  firstName: 'John',
+  lastName: 'Doe',
+  gender: 'M',
+  isRegistered: true,
+  address: {
+    city: 'Airmadidi',
+    postalCode: 95371,
+    province: 'Sulawesi Utara'
+  },
+  hobbies: ['berenang', 'traveling', 'membaca']
+};
+
+console.log(student?.address?.street);
+
+// optional chaining: ?.
