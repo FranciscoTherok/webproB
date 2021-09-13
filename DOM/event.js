601bd79486e96e3d9168a04081dc344f9ba563ee
reymonr <reymonr@unklab.ac.id>
2021-09-13 17:32:15 +0800

    DOM examples

---
+++ DOM/event.js
@@ -0,0 +1,16 @@
+// click
+const btn = document.querySelector('#buttonClick');
+btn.addEventListener('click', myEventHandler);
+
+function myEventHandler(event) {
+  console.log(event);
+
+  const h2 = document.querySelector('.sub-title');
+  h2.style.backgroundColor = 'red';
+  
+  console.log('Click event');
+}
+
+// mousemove
+
+// keyup
